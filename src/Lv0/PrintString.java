4cdd8d05857e0456991533724ee10385c876d21f
codedbyminjae <minjaekim7111@naver.com>
2025-09-05 10:20:27 +0900

    Lv0 - 문차출력

---
+++ src/Lv0/PrintString.java
@@ -0,0 +1,10 @@
+package Lv0;
+
+public class PrintString {
+    public static void main(String[] args) {
+        String message = "Let's go!";
+
+        System.out.println("3\n2\n1");
+        System.out.println(message);
+    }
+}
